738e1f99ee8ea3604176ced5ee832c2b9a49b708
Nohum <savva.gubarev228@mail.ru>
2025-09-04 23:58:24 +0300

    7

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -1,15 +1,17 @@
 package ru.yandex.practicum.filmorate.model;
 
-import jakarta.validation.constraints.*;
 import lombok.Data;
 import lombok.experimental.FieldDefaults;
+import ru.yandex.practicum.filmorate.validation.ValidReleaseDate;
 
+import jakarta.validation.constraints.*;
 import java.time.LocalDate;
 
 import static lombok.AccessLevel.PRIVATE;
 
 @Data
 @FieldDefaults(level = PRIVATE)
+@ValidReleaseDate
 public class Film {
     Integer id;
 
@@ -24,9 +26,4 @@ public class Film {
 
     @Positive(message = "Продолжительность фильма должна быть положительной.")
     Integer duration;
-
-    @AssertTrue(message = "Дата релиза не может быть раньше 28 декабря 1895 года.")
-    public boolean isReleaseDateValid() {
-        return releaseDate == null || !releaseDate.isBefore(LocalDate.of(1895, 12, 28));
-    }
 }
