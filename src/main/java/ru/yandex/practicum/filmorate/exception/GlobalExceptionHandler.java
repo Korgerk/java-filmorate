30a40f7ce7df2d5c81e8b63bdac69574918dd2aa
Nohum <savva.gubarev228@mail.ru>
2025-09-04 22:57:25 +0300

    2

--- src/main/java/ru/yandex/practicum/filmorate/exception/GlobalExceptionHandler.java
+++ src/main/java/ru/yandex/practicum/filmorate/exception/GlobalExceptionHandler.java
@@ -1,22 +1,24 @@
+// src/main/java/ru/yandex/practicum/filmorate/exception/ValidationExceptionHandler.java
+
 package ru.yandex.practicum.filmorate.exception;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
-    @ExceptionHandler(ValidationException.class)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public String handleValidationException(ValidationException e) {
-        return e.getMessage();
-    }
-
-    @ExceptionHandler(Exception.class)
-    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-    public String handleUnexpectedException(Exception e) {
-        return "Произошла ошибка: " + e.getMessage();
+    public Map<String, String> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getFieldErrors().forEach(error -> errors.put(error.getField(), error.getDefaultMessage()));
+        return errors;
     }
 }
