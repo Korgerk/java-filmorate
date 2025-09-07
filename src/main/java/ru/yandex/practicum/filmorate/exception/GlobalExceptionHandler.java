f7ef294b20558175671474e059c826abdea95d5c
Nohum <savva.gubarev228@mail.ru>
2025-09-07 23:19:05 +0300

    Update GlobalExceptionHandler.java

--- src/main/java/ru/yandex/practicum/filmorate/exception/GlobalExceptionHandler.java
+++ src/main/java/ru/yandex/practicum/filmorate/exception/GlobalExceptionHandler.java
@@ -2,15 +2,20 @@ package ru.yandex.practicum.filmorate.exception;
 
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.Collections;
+import java.util.List;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
-    @ExceptionHandler(ValidationException.class)
-    public ResponseEntity<String> handleValidationException(ValidationException e) {
-        return ResponseEntity.status(400).contentType(MediaType.TEXT_PLAIN).body(e.getMessage());
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<String> handleValidationException(MethodArgumentNotValidException e) {
+        List<String> errors = e.getBindingResult().getFieldErrors().stream().map(f -> f.getField() + ": " + f.getDefaultMessage()).toList();
+        return ResponseEntity.status(400).contentType(MediaType.TEXT_PLAIN).body(String.join(", ", Collections.singleton(errors)));
     }
 
     @ExceptionHandler(UserNotFoundException.class)
